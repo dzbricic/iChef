9f03a1bc00afc7088d73595d956d8f31ed13619b
mhadzic2 <user23691@5-159-229-148.rdns.melbourne.co.uk>
2014-08-22 18:16:02 +0100

    dan5

---
+++ jelo.h
@@ -0,0 +1,18 @@
+//
+//  jelo.h
+//  iChef
+//
+//  Created by user23691 on 22.8.2014..
+//  Copyright (c) 2014. dzm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jelo : NSObject
+
+@property NSString *tip;
+@property NSString *kategorija;
+@property NSString *naziv;
+@property NSString *recept;
+
+@end
---
+++ jelo.m
@@ -0,0 +1,13 @@
+//
+//  jelo.m
+//  iChef
+//
+//  Created by user23691 on 22.8.2014..
+//  Copyright (c) 2014. dzm. All rights reserved.
+//
+
+#import "jelo.h"
+
+@implementation jelo
+
+@end
