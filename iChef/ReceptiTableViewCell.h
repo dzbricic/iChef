1ffcfececc19b865e933e37433e557234a428793
dzbricic <user24011@77-240-12-6.rdns.melbourne.co.uk>
2014-08-24 10:39:54 +0100

    labels se ne moze povezati

---
+++ iChef/ReceptiTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  ReceptiTableViewCell.h
+//  iChef
+//
+//  Created by user24011 on 8/24/14.
+//  Copyright (c) 2014 dzm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReceptiTableViewCell :UITableViewCell
+
+
+@end
---
+++ iChef/ReceptiTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  ReceptiTableViewCell.m
+//  iChef
+//
+//  Created by user24011 on 8/24/14.
+//  Copyright (c) 2014 dzm. All rights reserved.
+//
+
+#import "ReceptiTableViewCell.h"
+
+@implementation ReceptiTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
